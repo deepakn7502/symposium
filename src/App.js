eb7a9251e7cf85df0b71d82a8aaa66b83a1a34b7
deepakn7502 <dostmeradeepak@gmail.com>
2023-02-27 13:23:08 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Register from "./Components/Register";
-import {} from "./firebase-config";
-import {} from "./server";
-import {} from "firebase/firestore";
+import { db } from "./firebase-config";
+import { accessData } from "./server";
+import { collection } from "firebase/firestore";
+import { } from "./server"
+import { Button } from "@mui/material";
 
 function App() {
+
+
+
+  const getData = async () => {
+
+    const databaseRef = collection(db, "sample");
+    const data = await accessData(databaseRef)
+    console.log(data)
+  }
+
   return (
     <div>
-      <Register />
+      {/* <Register /> */}
+      <Button onClick={() => { getData() }} variant="contained" >Get Data</Button>
     </div>
   );
 }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
-import { addDoc } from "firebase/firestore";
+import { addDoc, getDoc } from "firebase/firestore";
 
 export function Upload(databaseRef, data) {
   try {
     return addDoc(databaseRef, data).then(alert("Uploaded"));
   } catch (err) {
     return console.log(err.message);
+  }
+}
+
+export function accessData(databaseRef) {
+  try {
+    return getDoc(databaseRef)
+  } catch (error) {
+    console.log(error.message)
   }
 }
