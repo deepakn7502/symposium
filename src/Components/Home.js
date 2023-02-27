d61dcccdabe00a601b9c5bdbd5dd1d3930335a71
Arunkj203 <arunkj203@gmail.com>
2023-02-27 18:32:08 +0530

    animate

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,56 +3,6 @@ import "./Home.css";
 import logo from "../Images/logo.PNG";
 
 function Home() {
-
-    var words = ["Noir'23"],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 70;
-
-    var word=document.getElementById("name")
-
-let wordflick = () =>  {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    part = words[i].substr(0, offset);
-    if (skip_count == 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-    word.text(part);
-  },speed);
-};
-
-
-
-
     return (
         <div className="home">
             <div className="header">
@@ -74,7 +24,7 @@ let wordflick = () =>  {
 
             </div>
             <div className="symname">
-                <p id="name" onLoad={wordflick()}></p>
+                <p id="name" >NOIR'23</p>
             </div>
 
 
