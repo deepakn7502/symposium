1010e8743df691c5e303a9bdf2b695590c4cea98
Arunkumar.J <arunkj203@gmail.com>
2023-02-27 14:46:35 +0530

    260223

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Register from "./Components/Register";
-
 import { db } from "./firebase-config";
 import { accessData } from "./server";
 import { collection } from "firebase/firestore";
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,85 @@
 import React from "react";
 import "./Home.css";
+import logo from "../Images/logo.PNG";
 
-function Home(){
+function Home() {
 
-return(
-<div className="home">
-<h1>PANIMALAR ENGINEERING COLLEGE</h1>
-          {/* <img class="img" src="logo123.png" alt=" "></img> */}
-          <p>
-            <span>An Autonomous Institution</span>
-            <br />
-            Affliated to Anna University,Chennai
-            <br />
-            (JAISAKTHI EDUCATIONAL TRUST)
-          </p>
-</div>
-)
+    var words = ["Noir'23"],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+
+    var word=document.getElementById("name")
+
+let wordflick = () =>  {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    word.text(part);
+  },speed);
+};
+
+
+
+
+    return (
+        <div className="home">
+            <div className="header">
+                <div className="logo"><img class="img" src={logo} alt="" width="auto" height="150px" />
+                </div>
+
+                <div className="content">
+                    <h1 >PANIMALAR ENGINEERING COLLEGE </h1>
+
+
+                    <p>An Autonomous Institution</p>
+
+                    <p>Affliated to Anna University,Chennai</p>
+
+                    <p>JAISAKTHI EDUCATIONAL TRUST</p>
+
+                </div>
+
+
+            </div>
+            <div className="symname">
+                <p id="name" onLoad={wordflick()}></p>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Home;
