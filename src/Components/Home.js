e74ff6af01512f55d3017eecd6b7c30c1f357eed
Arunkj203 <arunkj203@gmail.com>
2023-02-28 13:37:01 +0530

    5454

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,26 +3,22 @@ import "./Home.css";
 import logo from "../Images/logo.png";
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="header">
-                <div className="logo"><img class="img" src={logo} alt="" width="auto" height="150px" />
-                </div>
+  return (
+    <div className="home">
+      <div className="header"> 
+        <div className="logo"><img class="img" src={logo} alt="" width="auto" height="150px" />   </div>
 
-                <div className="content">
-                <p id="name" >NOIR ON CAMPUS</p>
-
-                </div>
-
-
-            </div>
-            <div className="name">
-            <h1>PANIMALAR ENGINEERING COLLEGE </h1>
-            </div>
-
-
-        </div>
-    )
+        <div className="content">
+          <p id="name">NOIR ON CAMPUS</p>
+          <p id="tagname">AN INTERCOLLEGIATE SYMPOSIUM ON CRIME AND CULTURE </p>
+          </div>
+      </div>
+      <div className="name">
+        <h1>PANIMALAR ENGINEERING COLLEGE </h1>
+        <p>DEPARTMENT OF CSE</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
