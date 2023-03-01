c352307b8b023c6d6228b283f4a670966fe565c8
Arunkj203 <arunkj203@gmail.com>
2023-03-01 20:34:20 +0530

    final_out

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,10 @@
 import "./Card.css";
 import React, { useState, useRef } from "react";
-export default function Card({ ename, tagline,img }) {
+export default function Card({ ename, tagline, img }) {
   return (
-<<<<<<< HEAD
     <div className="card">
       <h1 className="title">{ename}</h1>
-      <img
-        src={img}
-        alt="Adzap"
-        className="sneaaker-img"
-      />
+      <img src={img} alt="Adzap" className="sneaaker-img" />
       <p className="tag">{tagline}</p>
       <div className="button-box">
         <button
@@ -20,38 +15,11 @@ export default function Card({ ename, tagline,img }) {
         >
           COMPETE HERE
         </button>
-=======
-    
-      <div
-        className="card"
-        
-        // onMouseMove={handleMouseMove}
-        // onMouseEnter={handleMouseEnter}
-        // onMouseLeave={handleMouseLeave}
-      >
-        <h1 className="title" >
-          {ename}
-        </h1>
-        <img
-          src={require("../Images/CmqvNiOUEAEPSW1.jpg")}
-          alt="Adzap"
-          className="sneaaker-img"
-        />
-        
-        <p className="tag"  >
-        {tagline}
-        </p>
-        
-        
-        <div className="button-box" >
-          <button className="purchase"  onClick={()=>{
-            window.location.pathname="/register"
-          }}>
-            COMPETE HERE
-          </button>
-        </div>
->>>>>>> b36a9f66ceb670f836d9ba989bc12b5bedd053f9
       </div>
     </div>
   );
 }
+
+// onMouseMove={handleMouseMove}
+// onMouseEnter={handleMouseEnter}
+// onMouseLeave={handleMouseLeave}
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,64 +1,70 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./Home.css";
 import { Button, TextField } from "@mui/material";
-import logo from "../Images/lgbt.png";
 import logo from "../Images/lg.png";
 import Card from "./Card";
 import FlipPage from "react-flip-page";
-import backgroundVideo from "../Images/bg.mp4"
+import backgroundVideo from "../Images/bg.mp4";
+
 import img1 from "../Images/card1.jpg";
 import img2 from "../Images/card1.jpg";
 import img3 from "../Images/card1.jpg";
 import img4 from "../Images/card1.jpg";
 import img5 from "../Images/card1.jpg";
 
-
-
 function Home() {
-
   return (
     <div className="home">
-      <div className="header">
-        <div className="logo">
-          <img src={logo} alt="" width="auto" height="100px" />
-        </div>
       <div className="main">
-      <video autoPlay  muted id='video'> <source src={backgroundVideo} type='video/mp4'/> </video>
+        <video autoPlay muted id="video">
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
         <div className="header">
           <div className="logo">
             <img src={logo} alt="" width="auto" height="100px" />
           </div>
-        </div>
-        
-   
-        <div className="content" >
-          <p id="name">NOIR ON CAMPUS</p>
-          <p id="tagname">AN INTERCOLLEGIATE SYMPOSIUM ON CRIME AND CULTURE </p>
-        </div>
-      </div>
 
+          <div className="content">
+            <p id="name">NOIR ON CAMPUS</p>
+            <p id="tagname">
+              AN INTERCOLLEGIATE SYMPOSIUM ON CRIME AND CULTURE{" "}
+            </p>
+          </div>
+        </div>
 
-      <div className="cardc1">
-        <Card ename="PIXEL IN THE SHADOWS (DALL-E AI)" tagline="DEII EPUDRAA KAILA VANDHUTU" img={img1}/>
-        <Card ename="SHERLOCK CODES" tagline="DEII EPUDRAA KAILA VANDHUTU" img={img2}/>
-        <Card ename="CRACK THE CASE" tagline="DEII EPUDRAA KAILA VANDHUTU" img={img3}/>
-      </div>
-      <div className="cardc2">
-        <Card
-          ename="FORTUNE 500"
-          tagline="Pitch it quick, make it stick - Adzap!" img={img4}
-        />
-        <Card ename="Yet to be decided" tagline="VAI ILLANA NAI THUKITU POIDUM" img={img5} />
+        <div className="cardc1">
+          <Card
+            ename="PIXEL IN THE SHADOWS (DALL-E AI)"
+            tagline="DEII EPUDRAA KAILA VANDHUTU"
+            img={img1}
+          />
+          <Card
+            ename="SHERLOCK CODES"
+            tagline="DEII EPUDRAA KAILA VANDHUTU"
+            img={img2}
+          />
+          <Card
+            ename="CRACK THE CASE"
+            tagline="DEII EPUDRAA KAILA VANDHUTU"
+            img={img3}
+          />
+        </div>
+
+        <div className="cardc2">
+          <Card
+            ename="FORTUNE 500"
+            tagline="Pitch it quick, make it stick - Adzap!"
+            img={img4}
+          />
+          <Card
+            ename="Yet to be decided"
+            tagline="VAI ILLANA NAI THUKITU POIDUM"
+            img={img5}
+          />
+        </div>
       </div>
     </div>
-
-  </div>
-    
-
   );
 }
 
 export default Home;
-
-
-
