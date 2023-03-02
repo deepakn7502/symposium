5ec84c230d08d8956a5409bbcfbf20ec2a3a6e3e
Arunkj203 <arunkj203@gmail.com>
2023-03-02 16:49:23 +0530

    final animations

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,6 @@ import Card from "./Card";
 import FlipPage from "react-flip-page";
 import backgroundVideo from "../Images/bg.mp4";
 
-
 import img1 from "../Images/dalleai.jpg";
 import img2 from "../Images/crack.jpg";
 import img3 from "../Images/coder.jpg";
@@ -14,16 +13,19 @@ import img4 from "../Images/fortune500.jpg";
 import img5 from "../Images/card1.jpg";
 
 function Home() {
+  const [header, setheader] = useState(false);
 
-  const [loaded, setLoaded] = useState(false);
-
+  const [cards, setcards] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setLoaded(true);
+      setheader(true);
     }, 10000);
-  }, []);
 
+    setTimeout(() => {
+      setcards(true);
+    }, 12000);
+  }, []);
 
   return (
     <div className="home">
@@ -31,54 +33,58 @@ function Home() {
         <video autoPlay muted id="video">
           <source src={backgroundVideo} type="video/mp4" />
         </video>
+        <div className="header">
+              <div className="logo">
+                <img src={logo} alt="" width="auto" height="100px" />
+              </div>
 
-        { loaded && 
-        <div>
-          <div className="header">
-          <div className="logo">
-            <img src={logo} alt="" width="auto" height="100px" />
+        {header && (
+          <div>
+          
+              <div className="content">
+                <p id="name">NOIR ON CAMPUS</p>
+                <p id="tagname">
+                  AN INTERCOLLEGIATE SYMPOSIUM ON CRIME AND CULTURE{" "}
+                </p>
+              </div>
+            </div>
+         
+        )}
+ </div>
+        {cards && (
+          <div>
+            <div className="cardc1">
+              <Card
+                ename="PIXEL IN THE SHADOWS (DALL-E AI)"
+                tagline="DEII EPUDRAA KAILA VANDHUTU"
+                img={img1}
+              />
+              <Card
+                ename="SHERLOCK CODES"
+                tagline="DEII EPUDRAA KAILA VANDHUTU"
+                img={img2}
+              />
+              <Card
+                ename="CRACK THE CASE"
+                tagline="DEII EPUDRAA KAILA VANDHUTU"
+                img={img3}
+              />
+            </div>
+
+            <div className="cardc2">
+              <Card
+                ename="FORTUNE 500"
+                tagline="Pitch it quick, make it stick - Adzap!"
+                img={img4}
+              />
+              <Card
+                ename="Yet to be decided"
+                tagline="VAI ILLANA NAI THUKITU POIDUM"
+                img={img5}
+              />
+            </div>
           </div>
-
-          <div className="content">
-            <p id="name">NOIR ON CAMPUS</p>
-            <p id="tagname">
-              AN INTERCOLLEGIATE SYMPOSIUM ON CRIME AND CULTURE{" "}
-            </p>
-          </div>
-        </div>
-        
-        <div className="cardc1">
-        <Card
-          ename="PIXEL IN THE SHADOWS (DALL-E AI)"
-          tagline="DEII EPUDRAA KAILA VANDHUTU"
-          img={img1}
-        />
-        <Card
-          ename="SHERLOCK CODES"
-          tagline="DEII EPUDRAA KAILA VANDHUTU"
-          img={img2}
-        />
-        <Card
-          ename="CRACK THE CASE"
-          tagline="DEII EPUDRAA KAILA VANDHUTU"
-          img={img3}
-        />
-      </div>
-
-      <div className="cardc2">
-        <Card
-          ename="FORTUNE 500"
-          tagline="Pitch it quick, make it stick - Adzap!"
-          img={img4}
-        />
-        <Card
-          ename="Yet to be decided"
-          tagline="VAI ILLANA NAI THUKITU POIDUM"
-          img={img5}
-        />
-      </div>
-      </div>
-        }
+        )}
       </div>
     </div>
   );
