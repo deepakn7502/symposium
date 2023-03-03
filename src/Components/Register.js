281f428e380537bcc6909221b7e97ce419c13c50
deepakn7502 <deepakn7502@gmail.com>
2023-03-03 08:32:37 +0530

    s

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -55,8 +55,45 @@ function Register() {
 
   return (
     <div className="register">
-      <Navbar />
-      <div className="inputs-container">
+      <div className="navbar1">
+        <div className="navbar">
+          <div className="name">PIXEL IN THE SHADOWS (DALL-E AI)</div>
+          <div className="cont">
+            <div className="container">
+              <div className="guide1">
+                <p className="gt"> GUIDELINES</p>• One team per college.
+                <br></br> • Each team will be given a topic ,where the two
+                different company's output or products will be given.
+                <br></br>• Your team has to infuse or integrate both the
+                products of different company and prepare an advertisement
+                regarding your hybrid product. <br></br>• For example, if the
+                topic is Dairy Milk and Boat Earbuds: U have to explain how u
+                combine both the products into an interesting product and make
+                an advertisement regarding the product.
+                <br></br> • After each and every team receives their respective
+                topics.<br></br> • Only a hour will be provided to prepare your
+                plan to advertise your product. <br></br>• At that span of time,
+                each team has to prepare and then record their advertisement
+                (Done through speech, U can also make use of props like charts,
+                art whatever u need , and everything needs to be recorded
+                properly).
+                <br></br> • Recorded video must be sent within a hour.<br></br>•
+                The recorded video must be within minimum 2 minutes to maximum 5
+                minutes, more than that is not advisable ( Make it crisp and
+                catchy, cause no one likes to watch ad for more than a
+                minute..cause we obviously know more duration makes ad boring,
+                So try not to be boring).<br></br> • Recorded content must have
+                clear audio and (video) and interference during the recording is
+                not appreciated.
+                <br></br> • The about mentioned are some major etiquette to be
+                followed. <br></br>• Team which doesn't stick to the above
+                mentioned points will be strictly disqualified.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="inputs-container">
         <div className="input-field">
           <TextField
             onChange={(e) => {
@@ -117,19 +154,19 @@ function Register() {
             />
           </Button>
         </div>
-      </div>
+      </div> */}
 
       {/* <Button variant="contained" onClick={getData}>Get data</Button>
       <Button variant="contained" onClick={uploadData}>Upload</Button> */}
 
       {/* <Button onClick={uploadImage}>Upload Image</Button> */}
 
-      <Button variant="contained" onClick={uploadImage}>
+      {/* <Button variant="contained" onClick={uploadImage}>
         Upload Image
       </Button>
       <Button variant="contained" onClick={uploadData}>
         Submit
-      </Button>
+      </Button> */}
     </div>
   );
 }
